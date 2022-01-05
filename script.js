43ffdb39f0aae8fef17a2fed3c1867e60b04ba45
IsacHolstad <wisac22@gmail.com>
2022-01-05 14:01:41 +0100

    assigments

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ var firstVar;
 var name = "isac";
 var number = "4";
 var division = 20 / 5;//4
+//alert (isac);
+var MyNumber = 7;
+var MyString = "dog";
+
+var inputIsvaild = false;
+if (inputIsvaild === false) {
+    alert ("please fill in the email field")}
+    else {
+        //do nothing....
+    }
+}
