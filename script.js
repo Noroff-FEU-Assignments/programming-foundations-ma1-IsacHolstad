47e5a9928c7ef65355a8d787e4c929c79546475b
IsacHolstad <wisac22@gmail.com>
2022-01-07 15:41:55 +0100

    complete

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 var firstVar;
+
+
 var name = "isac";
-console.log(name);
+
+
 var number = "4";
-console.log(number);
+
+
 var division = 20 / 5;//4
-console.log(division);
-//alert (isac);
+
+
+
+
 var animal = "frog";
 typeof(animal);
 console.log(animal);
+
 var orderHasShipped = true;
 var orderHasShipped = true;
 if (orderHasShipped){
