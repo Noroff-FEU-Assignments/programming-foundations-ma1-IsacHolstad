e4d8dec8c8bbede2b1e5864855aeca5605fa38a1
IsacHolstad <wisac22@gmail.com>
2022-01-07 15:17:25 +0100

    done

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ var name = "isac";
 var number = "4";
 var division = 20 / 5;//4
 //alert (isac);
-var MyNumber = 7;
-var MyString = "dog";
-
-var inputIsvaild = false;
-if (inputIsvaild === false) {
-    alert ("please fill in the email field")}
-    else {
-        //do nothing....
-    }
+var animal = "frog";
+typeof(animal);
+console.log(animal);
+var orderHasShipped = true;
+var orderHasShipped = true;
+if (orderHasShipped){
+    console.log("orderHasShipped");
+} 
+else {
+    console.log("orderNotShipped");
+}
+for (var count = 0; count <= 9; count++){
+    console.log(count);
 }
