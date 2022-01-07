b8d4e19c9aad093756b5a448902de7eab29557c8
IsacHolstad <wisac22@gmail.com>
2022-01-07 15:29:38 +0100

    complete

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var firstVar;
 var name = "isac";
+console.log(name);
 var number = "4";
+console.log(number);
 var division = 20 / 5;//4
+console.log(division);
 //alert (isac);
 var animal = "frog";
 typeof(animal);
